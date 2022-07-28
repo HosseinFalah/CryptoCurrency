22125d81cc72754bc2a1d66d2ea1d542d349c9d8
HosseinFalah <98707258+HosseinFalah@users.noreply.github.com>
2022-07-28 14:36:42 +0430

    set folder structure && layout

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+const App = () => {
+    return (
+        <div className="app">
+            <header className="navbar">
+
+            </header>
+            <main className="main">
+
+            </main>
+            <footer className="footer">
+
+            </footer>
+        </div>
+    )
+}
+
+export default App
